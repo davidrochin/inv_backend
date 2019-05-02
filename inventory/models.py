8575f9104cdb71ab52efb12578e7ef304eba998a
David Rochin <jdrc8@hotmail.com>
2019-05-01 21:00:10 -0600

    Se hicieron nulleables algunos campos

--- inventory/models.py
+++ inventory/models.py
@@ -11,8 +11,8 @@ class Item(models.Model):
     objects = ItemManager()
 
     name = models.CharField(max_length=200)
-    code = models.CharField(max_length=100)
+    code = models.CharField(max_length=100, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=8)
-    category_id = models.PositiveIntegerField()
-    provider_id = models.PositiveIntegerField()
+    category_id = models.PositiveIntegerField(null=True)
+    provider_id = models.PositiveIntegerField(null=True)
 
